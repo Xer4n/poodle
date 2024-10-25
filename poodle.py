6cab5d7052d19cea594ff6eed31bf3f56cfcde9f
Xer4n <simenandersen98@gmail.com>
2024-10-25 10:33:39 +0200

    Reading trace file works

--- poodle.py
+++ poodle.py
@@ -5,10 +5,20 @@ import sys
 
 file = sys.argv[1]
 
+def read_file(file):
+    accepted_lines = []
+    with open(file, "r") as f:
+        for line in f:
+            line = line.split(",")
+            if len(line) > 1:
+                line[1] = line[1].rstrip()
+                if (line[1] == "ACCEPTED"):
+                    print(line[1])
+        
+
 
 def main():
-    print("1")
-    print(file)
+    read_file(file)
 
 
 
