028a5b9e38356cd164f573261d5bb06db6a5d1a2
Xer4n <simenandersen98@gmail.com>
2024-10-25 10:21:21 +0200

    First commit

---
+++ poodle.py
@@ -0,0 +1,17 @@
+#Poodle attack by Simen Andersen
+#Email: SimAnd48374@stud.noroff.no
+
+import sys
+
+file = sys.argv[1]
+
+
+def main():
+    print("1")
+    print(file)
+
+
+
+
+if __name__ == "__main__":
+    main()
