57e4c1eaa65d5ed86f17595fb56dff2803bb1c9d
Xer4n <simenandersen98@gmail.com>
2024-10-25 11:49:02 +0200

    Almost finished

--- poodle.py
+++ poodle.py
@@ -1,6 +1,7 @@
 #Poodle attack by Simen Andersen
 #Email: SimAnd48374@stud.noroff.no
 
+from textwrap import wrap
 import sys
 
 file = sys.argv[1]
@@ -15,16 +16,42 @@ def read_file(file):
                 if (line[1] == "ACCEPTED"):
                     accepted_lines.append(line)
             elif len(line) == 1:
+                line[0] = line[0].rstrip()
                 accepted_lines.append(line)
         f.close()
 
     return accepted_lines
 
 
+def calculate_cookie(c_rminone, block_cr_minone, block_cn_minone):
+    c_nminone = len(block) - 1
+
+
+    block = wrap(block, 2)
+    
+    for i in range(len(block)-1):
+        pass
+    
+
 
 def main():
     traces = read_file(file)
+    cookie = []
+    
+    ssl_size = traces[0]
+    c_r = traces[1]
+    
+    c_rminone = int(c_r[0]) - 1
+
+    traces.pop(0)
+    traces.pop(1)
+
+    for line in traces:
+        blocks = line[0].split(" ")
         
+        for i in range(len(blocks)):
+            cookie.append(calculate_cookie(c_rminone, blocks[i-1][c_rminone], blocks[i-1][len(blocks[i-1]-1)]))
+     
 
 
 
